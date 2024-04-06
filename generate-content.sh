30897189ae5b0d7477b1398c68eda56079c8681a
Tung Bui <tung.bquang@gmail.com>
2024-04-06 18:06:20 +0700

    docs: keep it simple

--- generate-content.sh
+++ generate-content.sh
@@ -36,7 +36,7 @@ for service in $services; do
   youtube_id=$(echo "$youtube_url" | sed 's~https://youtu.be/~~')
 
   # Update the service README.md content
-  echo "| $id | $service_name | 📖 [$service_short_name]($url) | <img src="./assets/youtube_icon.png" height="20" width="20"> [youtu.be/$youtube_id](https://youtu.be/$youtube_id) | ❔ [$service_short_name/faqs]($faq_url)|" >>README.md
+  echo "| $id | $service_name | 📖 [$service_short_name]($url) | ▶️ [youtu.be/$youtube_id](https://youtu.be/$youtube_id) | ❔ [$service_short_name/faqs]($faq_url)|" >>README.md
 
   # Increment ID
   ((id++))
